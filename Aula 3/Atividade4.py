7ae04c626b6358b8006fa5a25b987d894b8b3182
DaviIshikawa <davicishikawa@outlook.com>
2026-03-09 16:39:05 -0300

    Update Atividade4.py

--- Aula 3/Atividade4.py
+++ Aula 3/Atividade4.py
@@ -1,26 +1,6 @@
 # Atividade 04
 
-print("Digite um filme/série")
-FILMESÉRIE = input()
+Ano = int(input("Digite o Ano: "))
 
-print("Digite um número de 1 a 5:")
-
-NUMERO = int(input())
-
-match NUMERO:
-    case 1:
-        print("Péssimo")
-        motivo = input("Por que você achou o filme Péssimo?")
-        print("Entendido, avaliação registrada")
-    case 2:
-        print("Ruim")
-        motivo = input("Por que você achou o filme ruim?")
-        print("Entendido, avaliação registrada")
-    case 3:
-        print("Razoável")
-    case 4:
-        print("Bom")
-    case 5:
-        print("Ótimo")
-    case _:
-        print("ALERTA! Número não identificado")
+Idade = 2040 - Ano
+print("O resultado é: ", Idade)
